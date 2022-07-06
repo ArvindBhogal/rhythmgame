d7ef3a3fea773bf90a9203a54794aec4eee9de2f
Arvind Bhogal <arvindbhogal@hotmail.com>
2022-07-05 20:52:18 -0400

    Added particle effects for hold notes

--- Assets/Scripts/LongNoteObject.cs
+++ Assets/Scripts/LongNoteObject.cs
@@ -10,11 +10,21 @@ public class LongNoteObject : MonoBehaviour
     public bool canBeReleased;
     public KeyCode keyToPress;
 
+    public ParticleSystem particleHeld;
+    public ParticleSystem particleHeld2;
+    public ParticleSystem particleHeld3;
+    public ParticleSystem particleHeld4;
+
     // Start is called before the first frame update
     void Start() {
         canBePressed = false;
         currentlyPressed = false;
         canBeReleased = false;
+
+        particleHeld = GameObject.Find("ParticleHeld").GetComponent<ParticleSystem>();
+        particleHeld2 = GameObject.Find("ParticleHeld (1)").GetComponent<ParticleSystem>();
+        particleHeld3 = GameObject.Find("ParticleHeld (2)").GetComponent<ParticleSystem>();
+        particleHeld4 = GameObject.Find("ParticleHeld (3)").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -23,6 +33,7 @@ public class LongNoteObject : MonoBehaviour
             if (canBePressed) {
                 currentlyPressed = true;
                 GameManager.instance.NoteHit();
+                OnParticleHold(keyToPress);
             }
         }
 
@@ -34,8 +45,39 @@ public class LongNoteObject : MonoBehaviour
                 GameManager.instance.NoteHit();
             }
             currentlyPressed = false;
+            OnParticleLetGo(keyToPress);
             Destroy(this);
             Destroy(gameObject);
+        }
+    }
+
+    private void OnParticleHold(KeyCode keyToPress) {
+        if (keyToPress == KeyCode.L) {
+            particleHeld.Play();
+        }
+        if (keyToPress == KeyCode.K) {
+            particleHeld2.Play();
+        }
+        if (keyToPress == KeyCode.F) {
+            particleHeld3.Play();
+        }
+        if (keyToPress == KeyCode.D) {
+            particleHeld4.Play();
+        }
+    }
+
+    private void OnParticleLetGo(KeyCode keyToPress) {
+        if (keyToPress == KeyCode.L) {
+            particleHeld.Stop();
+        }
+        if (keyToPress == KeyCode.K) {
+            particleHeld2.Stop();
+        }
+        if (keyToPress == KeyCode.F) {
+            particleHeld3.Stop();
+        }
+        if (keyToPress == KeyCode.D) {
+            particleHeld4.Stop();
         }
     }
 }
